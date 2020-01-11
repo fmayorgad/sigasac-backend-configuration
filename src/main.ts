725773f968c1954b17d4961f14b1d793fc70f5e2
aavillalbab <arithalexis@gmail.com>
2020-01-11 15:22:18 -0500

    general settings

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { DatabaseProvider } from 'sigasac-db';
+
+import { HttpExceptionFilter } from 'sigasac-utils';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    app.enableCors();
+
+    const options = new DocumentBuilder()
+        .setTitle('Siga SAC API, Configuration')
+        .setDescription(`${'módulo de configuraciones generales'}`)
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup(`${'configurations'}/apiDoc`, app, document);
+
+    await app.listen(3002, async () => {
+        const connection = await DatabaseProvider.getConnection();
+
+        const result = await connection.query('SELECT 1 + 1');
+
+        if (result.length && connection.isConnected) {
+            Logger.log(
+                `successful connection to the database ${connection.name}!`
+            );
+        }
+    });
+}
+bootstrap();
